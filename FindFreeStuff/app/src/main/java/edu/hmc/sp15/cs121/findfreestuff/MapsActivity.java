3a1e7921010aa242d2208ed87d77aca20a269686
SkylerWilliams <skyler.zee.williams@gmail.com>
2015-03-02 08:15:27 -0800

    Added Parse User functionality to the app. This included a Welcome Screen prompting the user for signin or signup, both of which have corresponding activities that communicate with the Parse backend. Added association of User and Free Item when creating a post, and when displaying the post information when the map marker is clicked. Cleaned up some old currently unused code.

--- FindFreeStuff/app/src/main/java/edu/hmc/sp15/cs121/findfreestuff/MapsActivity.java
+++ FindFreeStuff/app/src/main/java/edu/hmc/sp15/cs121/findfreestuff/MapsActivity.java
@@ -33,6 +33,7 @@ import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -94,6 +95,19 @@ public class MapsActivity extends FragmentActivity implements LocationListener,
             }
         });
 
+        // Set up the handler for the logout button click
+        Button logoutButton = (Button) findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Call the Parse log out method
+                ParseUser.logOut();
+                // Start and intent for the dispatch activity
+                Intent intent = new Intent(MapsActivity.this, DispatchActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
         displayFreeStuff();
     }
 
@@ -145,7 +159,7 @@ public class MapsActivity extends FragmentActivity implements LocationListener,
     private void setUpMap() {
         LatLng claremont = new LatLng(34.1067409,-117.7072027);
 
-        // Move initial view to Claremont, as the hardcoded free stuff is here
+        // Move initial view to Claremont, as emulator doesn't like initializing with GPS location
         mMap.setMyLocationEnabled(true);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(claremont, 13));
 
@@ -155,7 +169,6 @@ public class MapsActivity extends FragmentActivity implements LocationListener,
         // Sync down and display the Free Items from the Parse Core backend
         displayFreeStuff();
 
-        mMap.addMarker(new MarkerOptions().position(claremont).title("Marker"));
         //VERY IMPORTANT!!!
         //include this when we add clickable free item markers to the map screen
         //so that the ItemActivity can have a reference to the free item
@@ -185,7 +198,7 @@ public class MapsActivity extends FragmentActivity implements LocationListener,
 
                     // We will replace this toast with opening the Item Activity
                     Toast.makeText(MapsActivity.this,
-                            freeItem.getString("details"), Toast.LENGTH_SHORT).show();
+                           "Poster: " + freeItem.getParseUser("user").getUsername() + ". Details: " + freeItem.getString("details"), Toast.LENGTH_SHORT).show();
                     return;
                 // If the free item does not actually exist, remove it from our hash maps
                 // and remove its marker from the map
--- FindFreeStuff/app/src/main/java/edu/hmc/sp15/cs121/findfreestuff/PostActivity.java
+++ FindFreeStuff/app/src/main/java/edu/hmc/sp15/cs121/findfreestuff/PostActivity.java
@@ -76,7 +76,7 @@ public class PostActivity extends Activity {
             post.setLocation(geoPoint);
             post.setPostDetails(text);
             post.setPostTitle(title);
-            //post.setUser(ParseUser.getCurrentUser());
+            post.setUser(ParseUser.getCurrentUser());
             ParseACL acl = new ParseACL();
 
             // Give public read access
@@ -92,24 +92,6 @@ public class PostActivity extends Activity {
                 }
             });
         }
-    }
-
-    private String getPostEditTextTitleText () {
-        return postEditTextTitle.getText().toString().trim();
-    }
-
-    private String getPostEditTextDetailsText () {
-        return postEditTextDetails.getText().toString().trim();
-    }
-
-    private void updatePostButtonState () {
-        int length1 = getPostEditTextTitleText().length();
-        int length2 = getPostEditTextDetailsText().length();
-
-        //make sure they entered a title and description
-        //boolean enabled = length1 > 0 && length2 > 0;
-        boolean enabled = true;
-        postButton.setEnabled(enabled);
     }
 
 }
